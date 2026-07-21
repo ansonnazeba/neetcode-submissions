2df71ff94c48da2ee7bf542167ecd3a6363ce06d
Anson Nazeba <ansonnazeba@gmail.com>
2026-07-21 15:42:50 -0400

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,16 @@
+//import java.util.Collections;
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num: nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
