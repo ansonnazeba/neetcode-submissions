9f7598071a7bab7b732edf8918d4a7bb1332c68b
Anson Nazeba <ansonnazeba@gmail.com>
2026-07-22 07:44:01 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // loop through strings, and form a frequency map
+        // comapre the frequency maps
+        if (s.size() != t.size()) return false;
+        
+        unordered_map<char, int> s_map, t_map;
+
+        for (auto st: s) {
+            s_map[st] += 1;
+        }
+
+        for (auto st: t) {
+            t_map[st] += 1;
+        }
+
+        for (auto st: s) {
+            if (s_map[st] != t_map[st]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
