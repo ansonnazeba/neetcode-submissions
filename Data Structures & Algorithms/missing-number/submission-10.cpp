6997f99a190c44c1f5e2a3b8a51733d63444b7ec
Anson Nazeba <ansonnazeba@gmail.com>
2026-08-04 13:29:25 -0700

    Add: missing-number - submission-10

---
+++ Data Structures & Algorithms/missing-number/submission-10.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int givenSum = 0;
+        int n = nums.size();
+        
+        for (auto num: nums) {
+            givenSum += num;
+        }
+        
+        return (n * (n + 1) / 2) - givenSum;
+    }
+};
